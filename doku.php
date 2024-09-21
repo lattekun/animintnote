099c8f5496967d36245696d48e2fb9d691bc2638
lattekun <lu2005@naver.com>
2024-09-21 20:42:38 +0900

    Create doku.php

---
+++ doku.php
@@ -0,0 +1,5 @@
+<!DOCTYPE html>
+<meta charset="utf-8">
+<title>Redirecting to https://www.animint.net</title>
+<meta http-equiv="refresh" content="0; URL=https://www.animint.net">
+<link rel="canonical" href="https://www.animint.net">
